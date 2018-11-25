2c623cb219244d9dc59fbd879733f5539d485848
uhmtoto <uhmtotow@gmail.com>
2018-11-25 21:13:17 +0900

    DB Class

--- vote/db.py
+++ vote/db.py
@@ -12,4 +12,22 @@ class Candidate(db.Model):
     def __repr__ (self):
         return f"{self.id}|{self.name}|{self.part}|{self.phrase}|{self.image}"
 
+class Voter(db.Model):
+    __table_name__ = 'Voter'
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(7), nullable=False)
+
+    def __repr__ (self):
+        pass
+
+class Log(db.Model):
+    __table_name__ = 'Log'
+    id = db.Column(db.Integer, primary_key=True)
+    user_key = db.Column(db.String(7), nullable=False)
+    part = db.Column(db.String(50), nullable=False)
+    value = db.Column(db.Integer, nullable)
+
+    def __repr__ (self):
+        pass
+
 db.create_all()
